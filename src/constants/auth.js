d3a0be6cc45d924c6c44f41cb27f3fa501f85eef
Harishhk <hari850800@gmail.com>
2021-05-13 16:50:13 +0530

    favicon change

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,6 +1,6 @@
 module.exports={
-    url:'http://localhost:5050/api',
-    // url:"https://organize-me-age.herokuapp.com/api",
+    // url:'http://localhost:5050/api',
+    url:"https://organize-me-age.herokuapp.com/api",
     userId : "608ff4946eb6f200156c96d7",
     jwt : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDhmZjQ5NDZlYjZmMjAwMTU2Yzk2ZDciLCJpYXQiOjE2MjAwNDcyMTh9.x98uYIhrBjVuHre9xlyLFsF_leN2mIaliZYpTh1t2pg"
 
